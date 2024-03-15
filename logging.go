71bf4502e38803ace7d978a7ece6702d85e95502
hamilt79 <hamilt79@miamioh.edu>
2024-03-15 12:01:41 -0400

    Added comments, some functionality, and HasError function

--- logging.go
+++ logging.go
@@ -8,8 +8,12 @@ import (
 	"strings"
 )
 
+// mainLogger is the main logger that stores file and directory information
 var mainLogger Logger
 
+var errorExists bool
+
+// Logger is a struct that contains the log file name, the directory location and the full path of the log file
 type Logger struct
 {
     LogFileName string
@@ -17,10 +21,12 @@ type Logger struct
     fullPath string
 }
 
+// isWindows checks if the operating system is Windows
 func isWindows() bool {
     return runtime.GOOS == "windows"
 }
 
+// isValidDir checks if the directory exists
 func isValidDir(location string) bool {
     _, err := os.Stat(location)
     if err != nil {
@@ -29,6 +35,7 @@ func isValidDir(location string) bool {
     return true
 }
 
+// expandDirectory expands the directory location if it starts with the ~ directory
 func expandDirectory(location string) (string, error) {
     usr, err := user.Current()
     if err != nil {
@@ -46,6 +53,7 @@ func expandDirectory(location string) (string, error) {
     return location, nil
 }
 
+// makeLoggingDir creates the log directory
 func makeLoggingDir() error { 
     exists := isValidDir(mainLogger.DirLocation) 
     if (exists) {
@@ -56,6 +64,7 @@ func makeLoggingDir() error {
     return err
 }
 
+// makeLoggingFile creates the log file
 func makeLoggingFile() error {
     fileLocation := filepath.Join(mainLogger.DirLocation, mainLogger.LogFileName)
     mainLogger.fullPath = fileLocation
@@ -63,42 +72,55 @@ func makeLoggingFile() error {
     return err 
 }
 
+// HasError checks if an error has occurred in printing
+func HasError() bool {
+    return errorExists
+}
+
+// Print prints a string to the log file without adding a newline character
 func Print(str string) {
     file, err := os.OpenFile(mainLogger.fullPath, os.O_APPEND|os.O_WRONLY, 0644)
     if (err != nil) {
-        panic(err)
+        errorExists = true
     }
     file.WriteString(str)
     file.Close()
 }
 
+// PrintLn prints a string to the log file and adds a newline character
 func PrintLn(str string) {
     file, err := os.OpenFile(mainLogger.fullPath, os.O_APPEND|os.O_WRONLY, 0644)
     if (err != nil) {
-        panic(err)
+        errorExists = true
     }
     file.WriteString(str + "\n")
     file.Close()
 }
 
-func Init(logger Logger) {
+// Init initializes the logger
+// Uses the Logger struct to set the directory location and the log file name
+func Init(logger Logger) error {
+    errorExists = false
+    // Expanding the directory location if it starts with the ~ directory
+    // $HOME is not supported and ~ is only supported in the beginning of the string
     newLocation, err := expandDirectory(logger.DirLocation)
-    if err != nil {
-        panic(err)
+    if err != nil { 
+        return err
     }
+    // Setting the new directory location
     logger.DirLocation = newLocation
+    // Setting the mainLogger
     mainLogger = logger
+    // Making the directory that should be logged to
     err = makeLoggingDir()
     if err != nil {
-        panic(err)
+        return err
     }
+    // Making the file that should be logged to
     err = makeLoggingFile()
     if err != nil {
-        panic(err)
+        return err
     }
+    return nil
 }
 
-func main() {
-    logger := Logger { LogFileName: "TestLogg.txt", DirLocation: "~/.logging/"} 
-    Init(logger)
-}
